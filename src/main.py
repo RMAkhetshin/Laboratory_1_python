599f7182c20ab5dc0a9fb553fb246f7e31e861ad
RMAkhmetshin <parus-rg52@mail.ru>
2025-10-10 23:37:38 +0400

    started development in branch'develop'

--- src/main.py
+++ src/main.py
@@ -1,20 +1,55 @@
-from src.power import power_function
-from src.constants import SAMPLE_CONSTANT
+# from src.power import power_function
+# from src.constants import SAMPLE_CONSTANT
 
 
-def main() -> None:
-    """
-    Обязательнная составляющая программ, которые сдаются. Является точкой входа в приложение
-    :return: Данная функция ничего не возвращает
-    """
+# def main() -> None:
+#     """
+#     Обязательнная составляющая программ, которые сдаются. Является точкой входа в приложение
+#     :return: Данная функция ничего не возвращает
+#     """
 
-    target, degree = map(int, input("Введите два числа разделенные пробелом: ").split(" "))
+#     target, degree = map(int, input("Введите два числа разделенные пробелом: ").split(" "))
 
-    result = power_function(target=target, power=degree)
+#     result = power_function(target=target, power=degree)
 
-    print(result)
+#     print(result)
 
-    print(SAMPLE_CONSTANT)
+#     print(SAMPLE_CONSTANT)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+import re
+# expression = input()
+text = "  12 + 3.5 * -2  "
+
+TOKEN_RE = re.compile(r"""
+\s*
+(
+  [+-]?\d+(?:\.\d+)?
+  | [+\-*/]
+)
+""", re.VERBOSE)
+
+for index, op_match in enumerate(TOKEN_RE.finditer(text)):
+  print("# match is {0}, match is {1}".format(index, op_match))
+  for group in op_match.groups():
+    print("Finded group: ", group)
+
+def expr():
+    ...
+
+def add():
+    ...
+
+def mul():
+    ...
+
+def pow():
+    ...
+
+def unary():
+    ...
+
+def primary():
+    ...
---
+++ src/my_M3.py
@@ -0,0 +1,75 @@
+# Калькулятор М3
+
+class CalcError(Exception):
+    # Класс для обработки ошибок калькулятора
+    pass
+
+def main_function(expression: str) -> float:
+    # Главная функция программы -- подсчёт выражения в RPN
+    if not expression:
+        raise CalcError("Пустое выражение")
+    if expression.count("(") > expression.count(")"):
+        raise CalcError(f"Не закрыто {expression.count("(") - expression.count(")")} скобок")
+    elif expression.count("(") < expression.count(")"):
+        raise CalcError(f"Не открыто {expression.count(")") - expression.count("(")} скобок")
+    # if
+
+    for i in "+-%*/()":
+        expression = expression.replace(i, f" {i} ")
+
+    expression = expression.replace("/  /", "//")
+    expression = expression.replace("*  *", "**")
+    expression = expression.replace("(", "")
+    expression = expression.replace(")", "")
+
+    tokens = list(expression.split())
+
+    stack = []
+    for token in tokens:
+        if token.replace(".", "").isdigit() and token.count(".") <= 1:
+            stack.append(float(token))
+
+        elif token in ['+', '-', '*', '/', '//', '%', '**']:
+            if len(stack) < 2:
+                raise CalcError(f"Недостаточно операндов для оператора {token}")
+            
+            else:
+                element_2 = stack.pop()
+                element_1 = stack.pop()
+
+                if token == '+':
+                    result = element_1 + element_2
+                elif token == '-':
+                    result = element_1 - element_2
+                elif token == '*':
+                    result = element_1 * element_2
+                elif token == '/':
+                    if element_2 == 0:
+                        raise CalcError("Деление на ноль")
+                    result = element_1 / element_2
+                elif token == '//':
+                    if element_2 == 0:
+                        raise CalcError("Целочисленное деление на ноль")
+                    result = element_1 // element_2
+                elif token == '%':
+                    if element_2 == 0:
+                        raise CalcError("Остаток от деления на ноль")
+                    result = element_1 % element_2
+                elif token == '**':
+                    result = element_1 ** element_2
+            stack.append(float(result))
+
+        else:
+            raise CalcError(f"Неизвестный токен: '{token}'")
+
+    if len(stack) == 1:
+        return stack[0]
+    # elif len(stack) > 1:
+    else:
+        raise CalcError(f"Не хватает операторов: {stack} -- оставшиеся числа")
+
+
+
+
+
+print(main_function("(23 (3 4 -) +)"))
